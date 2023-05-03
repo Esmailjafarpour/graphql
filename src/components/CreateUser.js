4c5930cc981d0bfeba6ca3b19847807bedbd0cb4
nader <esmailjafarpour29@gmail.com>
2023-05-03 17:15:27 +0330

    Passing user information to the useMutation hook and dynamizing the information that goes to the useMutation hook using useState

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React , {useState} from 'react';
 import {gql , useMutation} from '@apollo/client';
 
 const CREATE_USER = gql`
-     mutation{
+     mutation createUser(
+          $name : String!,
+          $username : String!,
+          $email : String!,
+          $phone : String!,
+     ){
           createUser(
                input : {
-                    name : "nader",
-                    username : "jafarpour",
-                    email : "esmailJafarpour29@gmail.com",
-                    phone : "09176414952",
-
+                    name : $name,
+                    username : $username,
+                    email : $email,
+                    phone : $phone,
                }
           ){
                id,
@@ -23,10 +27,45 @@ const CREATE_USER = gql`
 
 const CreateUser = () => {
 
-     const [createUser,{loading , data , error , called}] = useMutation(CREATE_USER);
+     const [state, setAllState] = useState({
+          name : "",
+          username : "",
+          email : "",
+          phone : ""
+     });
+
+     const changeHandler = (event) => {
+
+          setAllState({
+               ...state,
+               [event.target.name] : event.target.value 
+
+               } 
+          )
+     }
+
+     const [createUser,{loading , data , error , called}] = useMutation(
+
+          CREATE_USER,{
+
+               variables : {
+                    name : state.name,
+                    username : state.nameuser,
+                    email : state.email,
+                    phone : state.phone,  
+               }
+              
+          }
+     );
      console.log({loading , data , error , called})
      return (
           <div>
+               <div>
+                    <input type="text" name="name" value={state.name} onChange={(event) => changeHandler(event)}/>
+                    <input type="text" name="nameuser" value={state.nameuser} onChange={(event) => changeHandler(event)}/>
+                    <input type="text" name="email" value={state.email} onChange={(event) => changeHandler(event)}/>
+                    <input type="text" name="phone" value={state.phone} onChange={(event) => changeHandler(event)}/>
+               </div>
                 <button onClick={()=> createUser()}>Get User</button>
           </div>
      );
